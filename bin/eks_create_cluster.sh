9f34cacdd22d523090948f8443f42e77c4f7320e
Nhan <hdtnhan91@gmail.com>
2023-07-01 22:16:55 +0700

    try

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CLUSTER="production"
-REGION="us-east-1"
+REGION="us-west-2"
 CLUSTER_NAME="${CLUSTER}.${REGION}.eksctl.io"
 
 if kubectl config get-clusters | grep -q ${CLUSTER_NAME}; then
